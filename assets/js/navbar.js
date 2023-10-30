afdd55d18ef6f5378f25dfa4654411d2ef2ad57f
AJStrong <90976178+ajstrongdev@users.noreply.github.com>
2023-10-30 15:09:45 +0000

    Initial commit
    - Create index.html
    - Modify index.html
    - Create /assets/css/style.css
    - Modify styles.css file for styling
    - Publish images.

---
+++ assets/js/navbar.js
@@ -0,0 +1,8 @@
+// Show mobile menu
+function showMobileMenu() {
+    document.getElementById("mobile-nav").style.display = "block";
+}
+// Hide mobile menu
+function hideMobileMenu() {
+    document.getElementById("mobile-nav").style.display = "none";
+}
